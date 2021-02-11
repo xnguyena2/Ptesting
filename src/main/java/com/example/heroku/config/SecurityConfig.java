445ea21b45bce6c60bbe5357c570d85ca595fb15
xnguyena2 <xnguyena2@yahoo.com>
2021-02-11 16:37:49 +0700

    [real test bootstrap data]

--- src/main/java/com/example/heroku/config/SecurityConfig.java
+++ src/main/java/com/example/heroku/config/SecurityConfig.java
@@ -7,19 +7,23 @@ import com.example.heroku.model.repository.UserRepository;
 import lombok.var;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.ServerAuthenticationEntryPoint;
 import org.springframework.security.web.server.authentication.RedirectServerAuthenticationEntryPoint;
 import org.springframework.security.web.server.authorization.AuthorizationContext;
 import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Configuration
@@ -69,8 +73,11 @@ public class SecurityConfig {
                 .addFilterAt(new JwtTokenAuthenticationFilter(tokenProvider), SecurityWebFiltersOrder.HTTP_BASIC)
 
                 .exceptionHandling()
-                .authenticationEntryPoint(new RedirectServerAuthenticationEntryPoint("/login"))
+                .authenticationEntryPoint((ServerAuthenticationEntryPoint) (exchange, e) -> {
+                    exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                })
                 .and()
+
                 .build();
     }
 
