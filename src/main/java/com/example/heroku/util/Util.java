618b551e6ff32c5111125c52666ed37f752f0c1f
xnguyena2 <xnguyena2@yahoo.com>
2021-03-31 17:53:31 +0700

    [fix vietname acennt]

--- src/main/java/com/example/heroku/util/Util.java
+++ src/main/java/com/example/heroku/util/Util.java
@@ -120,12 +120,44 @@ public class Util {
         return ch;
     }
 
+    public String convert(String str) {
+
+
+        str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+
+        str = str.replaceAll("à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ", "a");
+        str = str.replaceAll("è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ", "e");
+        str = str.replaceAll("ì|í|ị|ỉ|ĩ", "i");
+        str = str.replaceAll("ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ", "o");
+        str = str.replaceAll("ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ", "u");
+        str = str.replaceAll("ỳ|ý|ỵ|ỷ|ỹ", "y");
+        str = str.replaceAll("đ", "d");
+
+        str = str.replaceAll("À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ", "A");
+        str = str.replaceAll("È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ", "E");
+        str = str.replaceAll("Ì|Í|Ị|Ỉ|Ĩ", "I");
+        str = str.replaceAll("Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ", "O");
+        str = str.replaceAll("Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ", "U");
+        str = str.replaceAll("Ỳ|Ý|Ỵ|Ỷ|Ỹ", "Y");
+        str = str.replaceAll("Đ", "D");
+
+        str = str.replace("\u0300|\u0301|\u0303|\u0309|\u0323", ""); // ̀ ́ ̃ ̉ ̣  huyền, sắc, ngã, hỏi, nặng
+        str = str.replace("\u02C6|\u0306|\u031B", ""); // ˆ ̆ ̛  Â, Ê, Ă, Ơ, Ư
+        str = str.replace("  ", " ");
+        str = str.trim();
+
+        return str;
+    }
+
     public String RemoveAccent(String str) {
+        return convert(str);
+        /*
         StringBuilder sb = new StringBuilder(str);
         for (int i = 0; i < sb.length(); i++) {
             sb.setCharAt(i, RemoveAccent(sb.charAt(i)));
         }
         return sb.toString().toLowerCase();
+        */
     }
 
     public int DiffirentDays(Timestamp t1, Timestamp current) {
