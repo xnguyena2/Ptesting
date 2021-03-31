b3b7e3bbcd46b6b5bffbf2d04a727cc1d2d4d728
xnguyena2 <xnguyena2@yahoo.com>
2021-03-31 16:50:30 +0700

    [distinc beerorder]

--- src/main/java/com/example/heroku/services/PackageOrder.java
+++ src/main/java/com/example/heroku/services/PackageOrder.java
@@ -1,5 +1,6 @@
 package com.example.heroku.services;
 
+import com.example.heroku.model.BeerOrder;
 import com.example.heroku.model.repository.BeerOrderRepository;
 import com.example.heroku.model.repository.BeerUnitOrderRepository;
 import com.example.heroku.model.repository.PackageOrderRepository;
@@ -48,6 +49,7 @@ public class PackageOrder {
                 .map(OrderSearchResult.PackageOrderData::new)
                 .flatMap(packageOrderData ->
                         beerOrderRepository.findBySecondID(packageOrderData.getPackage_order_second_id())
+                                .distinct(BeerOrder::getBeer_second_id)
                                 .map(OrderSearchResult.PackageOrderData.BeerOrderData::new)
                                 .map(packageOrderData::Add)
                                 .flatMap(beerOrderData ->
