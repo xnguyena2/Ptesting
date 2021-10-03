45785e84392b18f9c92b369c3e843bb6ddce6ca9
xnguyena2 <xnguyena2@yahoo.com>
2021-10-03 19:33:09 +0700

    [test for dev]

--- src/main/java/com/example/heroku/services/BeerOrder.java
+++ src/main/java/com/example/heroku/services/BeerOrder.java
@@ -239,7 +239,6 @@ public class BeerOrder {
                 );
     }
 
-    //for trum bien
     public Mono<PackageOrder> createOrderTrumBien(PackageOrderData packageOrderData) {
         System.out.println("New request from: " + packageOrderData.getPackageOrder().getUser_device_id());
         packageOrderData.getPackageOrder().AutoFill(packageOrderData.isPreOrder());
