abdf19644a13a7909f53dfe7788a5c5c54097825
xnguyena2 <xnguyena2@yahoo.com>
2023-09-15 16:43:08 +0700

    [add group_id to fetch package]

--- src/main/java/com/example/heroku/model/UserPackage.java
+++ src/main/java/com/example/heroku/model/UserPackage.java
@@ -33,6 +33,7 @@ public class UserPackage extends BaseEntity {
 
     public UserPackage(UserPackage s){
         this.id = s.id;
+        this.group_id = s.group_id;
         this.device_id = s.device_id;
         this.product_second_id = s.product_second_id;
         this.product_unit_second_id = s.product_unit_second_id;
