b0f3e9f09163feab8a59aea7470d26bcfcdc91f5
xnguyena2 <xnguyena2@yahoo.com>
2023-12-09 12:14:52 +0700

    [fix group id]

--- src/main/java/com/example/heroku/model/repository/StatisticTotalBenifitRepository.java
+++ src/main/java/com/example/heroku/model/repository/StatisticTotalBenifitRepository.java
@@ -10,6 +10,6 @@ import reactor.core.publisher.Mono;
 import java.sql.Timestamp;
 
 public interface StatisticTotalBenifitRepository extends ReactiveCrudRepository<BenifitByMonth, Long> {
-    @Query(value = "SELECT COALESCE(SUM(cost), 0) AS cost, COALESCE(SUM(profit), 0) AS profit, COALESCE(SUM(payment), 0) AS revenue, COUNT(*) AS count FROM user_package_detail WHERE user_package_detail.group_id = :group_id AND user_package_detail.status = :status AND (user_package_detail.createat AT TIME ZONE 'ICT' BETWEEN :fromtime AND :totime)")
+    @Query(value = "SELECT COALESCE(SUM(cost), 0) AS cost, COALESCE(SUM(profit), 0) AS profit, COALESCE(SUM(payment), 0) AS revenue, COUNT(*) AS count FROM user_package_detail WHERE user_package_detail.group_id = :group_id AND user_package_detail.status = :status AND (user_package_detail.createat AT TIME ZONE '+07' BETWEEN :fromtime AND :totime)")
     Mono<BenifitByMonth> getTotalStatictis(@Param("group_id") String groupID, @Param("fromtime") Timestamp from, @Param("totime") Timestamp to, @Param("status") UserPackageDetail.Status status);
 }
