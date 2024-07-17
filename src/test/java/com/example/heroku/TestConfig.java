d74dd763da307e8d817b5c9b041ea333e5258a27
xnguyena2 <xnguyena2@yahoo.com>
2024-07-17 08:06:06 +0700

    [update database]

--- src/test/java/com/example/heroku/TestConfig.java
+++ src/test/java/com/example/heroku/TestConfig.java
@@ -10,12 +10,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 @TestPropertySource(properties = {
         "FIRE_BASE_TOKEN=",
-        "JDBC_DATABASE_URL=jdbc:postgres://dpg-cpj6iaq1hbls73bqu72g-a.singapore-postgres.render.com/sale_management_dev_7kqj",
+        "JDBC_DATABASE_URL=jdbc:postgres://dpg-cqbh2k5umphs73d45a80-a.singapore-postgres.render.com/sale_management_dev_wqik",
         "POSTGRESQL_PORT=5432",
-        "POSTGRESQL_DB=sale_management_dev_7kqj",
-        "POSTGRESQL_HOST=dpg-cpj6iaq1hbls73bqu72g-a.singapore-postgres.render.com",
-        "DB_USER_NAME=sale_management_dev_7kqj_user",
-        "DB_PASSWORD=9O8JrfdxfhI66AKZPY0K9eTRwKLToZeA",
+        "POSTGRESQL_DB=sale_management_dev_wqik",
+        "POSTGRESQL_HOST=dpg-cqbh2k5umphs73d45a80-a.singapore-postgres.render.com",
+        "DB_USER_NAME=sale_management_dev_wqik_user",
+        "DB_PASSWORD=AY4EfscbfT5ZNDzLx4PTyoz4vbDaBXN5",
         "DB_DRIVER=org.postgresql.Driver",
         "POSTGRESQL_POOLSIZE=10",
         "POSTGRESQL_TIMEIDLE=10000",
