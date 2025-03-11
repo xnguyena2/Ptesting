808ec120bd7f507fbff90aa3602baf0aff12d354
xnguyena2 <xnguyena2@yahoo.com>
2025-03-11 20:33:15 +0700

    [update query all debt of buyer]

--- src/main/java/com/example/heroku/model/repository/StatisticDebtOfBuyerRepository.java
+++ src/main/java/com/example/heroku/model/repository/StatisticDebtOfBuyerRepository.java
@@ -8,6 +8,6 @@ import reactor.core.publisher.Flux;
 
 public interface StatisticDebtOfBuyerRepository extends ReactiveCrudRepository<DebtOfBuyer, Long> {
 
-    @Query(value = "SELECT buyer.*, debt.in_come AS in_come, debt.out_come AS out_come FROM (SELECT device_id, SUM(CASE WHEN transaction_type = 'INCOME' THEN amount ELSE 0 END) AS in_come, SUM(CASE WHEN transaction_type = 'OUTCOME' THEN amount ELSE 0 END) AS out_come FROM debt_transaction WHERE group_id = :group_id GROUP BY device_id) AS debt LEFT JOIN (SELECT * FROM buyer WHERE group_id = :group_id) AS buyer ON debt.device_id = buyer.device_id")
+    @Query(value = "SELECT buyer.*, debt.in_come AS in_come, debt.out_come AS out_come FROM (SELECT device_id, SUM(CASE WHEN transaction_type = 'INCOME' THEN amount ELSE 0 END) AS in_come, SUM(CASE WHEN transaction_type = 'OUTCOME' THEN amount ELSE 0 END) AS out_come FROM debt_transaction WHERE group_id = :group_id GROUP BY device_id) AS debt INNER JOIN (SELECT * FROM buyer WHERE group_id = :group_id) AS buyer ON debt.device_id = buyer.device_id")
     Flux<DebtOfBuyer> getIncomeOutComeAllBuyer(@Param("group_id") String groupID);
 }
