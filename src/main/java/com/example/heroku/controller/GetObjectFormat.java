c16418eb9a54d65e383c93add9747f802ab2ff78
xnguyena2 <xnguyena2@yahoo.com>
2025-03-16 21:16:57 +0700

    [add send fmc to admin request login code]

--- src/main/java/com/example/heroku/controller/GetObjectFormat.java
+++ src/main/java/com/example/heroku/controller/GetObjectFormat.java
@@ -1,8 +1,10 @@
 package com.example.heroku.controller;
 
+import com.example.heroku.firebase.MyFireBase;
 import com.example.heroku.request.logs.Logs;
 import com.example.heroku.response.Format;
 import com.example.heroku.util.Util;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
@@ -14,10 +16,18 @@ import static org.springframework.http.ResponseEntity.ok;
 @RestController
 @RequestMapping("/getobjectjs")
 public class GetObjectFormat {
+
+    @Autowired
+    MyFireBase myFireBase;
+
     @PostMapping("/logs")
     @CrossOrigin(origins = Util.HOST_URL)
     public Mono<ResponseEntity> getPackageObj(@RequestBody @Valid Logs logs) {
         System.out.println(logs.getLogs());
+        if (logs.getLogs() != null && logs.getLogs().startsWith("******************* Request login code from phone: ")) {
+            String phone = logs.getLogs().substring(48);
+            myFireBase.SendNotification2Admin("Request login code from phone: " + phone, "Request login code from phone: " + phone);
+        }
         return
                 Mono.just(ok(Format.builder().response("Done!").build()));
     }
