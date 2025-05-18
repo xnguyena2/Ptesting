6a60697b5c69b82cb94dccaa34824e448186e94a
xnguyena2 <xnguyena2@yahoo.com>
2025-05-18 14:13:39 +0700

    [add external url]

--- src/test/java/com/example/heroku/TestConfig.java
+++ src/test/java/com/example/heroku/TestConfig.java
@@ -13,7 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
         "JDBC_DATABASE_URL=jdbc:postgresql://dpg-d0ko3mjuibrs739mkrng-a.singapore-postgres.render.com/sale_management_dev_m5rd",
         "POSTGRESQL_PORT=5432",
         "POSTGRESQL_DB=sale_management_dev_m5rd",
-        "POSTGRESQL_EXTERNAL_URL=r2dbc:postgresql://sale_management_dev_m5rd_user:lwmnzH0im5bxfwz9pMpucfNj6J46l690@dpg-d0ko3mjuibrs739mkrng-a.singapore-postgres.render.com/sale_management_dev_m5rd",
+        "POSTGRESQL_EXTERNAL_URL=postgresql://sale_management_dev_m5rd_user:lwmnzH0im5bxfwz9pMpucfNj6J46l690@dpg-d0ko3mjuibrs739mkrng-a.singapore-postgres.render.com/sale_management_dev_m5rd",
         "POSTGRESQL_HOST=dpg-d0ko3mjuibrs739mkrng-a.singapore-postgres.render.com",
         "DB_USER_NAME=sale_management_dev_m5rd_user",
         "DB_PASSWORD=lwmnzH0im5bxfwz9pMpucfNj6J46l690",
