2b8cbeb2247d058fc6a3434c28a269aa767c7790
xnguyena2 <xnguyena2@yahoo.com>
2025-09-18 09:42:48 +0700

    [update db]

--- src/test/java/com/example/heroku/TestConfig.java
+++ src/test/java/com/example/heroku/TestConfig.java
@@ -13,7 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
         "JDBC_DATABASE_URL=jdbc:postgresql://dpg-d0ko3mjuibrs739mkrng-a.singapore-postgres.render.com/sale_management_dev_m5rd",
         "POSTGRESQL_PORT=5432",
         "POSTGRESQL_DB=sale_management_dev_m5rd",
-        "POSTGRESQL_EXTERNAL_URL=r2dbc:postgresql://sale_management_dev_cid3_user:F031SGLRC0uRYaoQfjwZdLf2Uh1gFPHC@dpg-d2hitsi4d50c739evh30-a.singapore-postgres.render.com/sale_management_dev_cid3?sslmode=require",
+        "POSTGRESQL_EXTERNAL_URL=r2dbc:postgresql://sale_management_dev_ncw9_user:tPGeZxUfrRZFV0rJ7GHSz1ZNTdxVNAjq@dpg-d35murnfte5s7392dhn0-a.singapore-postgres.render.com/sale_management_dev_ncw9?sslmode=require",
         "POSTGRESQL_HOST=dpg-d0ko3mjuibrs739mkrng-a.singapore-postgres.render.com",
         "DB_USER_NAME=sale_management_dev_m5rd_user",
         "DB_PASSWORD=lwmnzH0im5bxfwz9pMpucfNj6J46l690",
